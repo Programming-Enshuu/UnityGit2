d5e25d3a6dbbb763e417b71d574fddc80ff7c9f0
Unity <information.j4@gmail.com>
2019-11-15 12:14:28 +0900

    Revert "All of unity project files"
    This reverts commit a206aaf9208fa1c124b311d4d386c191b0513731.

--- takenokko_to_kinokko3/Assets/Program/ObjectCollision.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObjectCollision : MonoBehaviour
-{
-    public float boundPower;
-    public bool playerStepOn = false;
-}
